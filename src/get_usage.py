ec995e46901f78722e4300ecb0d6769fb274951d
kgero <katystreet@gmail.com>
2019-01-18 13:41:48 -0500

    slight improvement on sentence selection

--- src/get_usage.py
+++ src/get_usage.py
@@ -3,7 +3,7 @@ Functions for getting examples of word usage.
 """
 import os
 
-STYLES = ['darwin', 'dickens', 'food', 'joyce', 'poetry', 'sherlock']
+STYLES = ['darwin', 'dickens', 'food', 'joyce', 'poetry', 'sherlock', 'law']
 
 def get_ex_sen(word, style):
     """Return string of example usage of word in style corpus."""
@@ -11,8 +11,21 @@ def get_ex_sen(word, style):
         return {'dicterror': 'no corpus available for {}.'.format(style)}
     fldr = os.path.join('dat', style)
     for fname in os.listdir(fldr):
+        prevline = ''
         for line in open(os.path.join(fldr, fname), errors='ignore'):
             clean = line.lower()
             if word in clean:
-                return {'sentence': line}
+                if len(line) > 150:
+                    sentences = line.split('. ')
+                    for sen in sentences:
+                        if word in sen.lower():
+                            return {'sentence': sen + '.'}
+                elif len(clean.split(word)[0]) < 10:
+                    sentences = (prevline + line).split('. ')
+                    for sen in sentences:
+                        if word in sen.lower():
+                            return {'sentence': sen + '.'}
+                else:
+                    return {'sentence': line}
+            prevline = line
     return {'dicterror': "didn't find word in corpus"}
