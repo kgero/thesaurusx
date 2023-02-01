54ac886df74d2be1d01371e59292d79325bf5d26
Katy Macbook Pro <katygero@Katys-MacBook-Pro.local>
2023-02-01 14:47:37 -0500

    deal with no word found

--- server.py
+++ server.py
@@ -21,6 +21,25 @@ def get_words_thes():
 def get_words_algo():
     rform = request.form
     data = simple_lookup(rform['keyword'], embkey=rform['embd'])
+
+    # deal with no word in embeddings
+    if 'error' in data:
+        found = False
+        sub_data = thesaurus_lookup(request.form['keyword'])
+        if 'results' in sub_data:
+            for key, val in sub_data['results'].items():
+                if key == 'pos':
+                    continue
+                if found:
+                    break
+                for i in range(3):
+                    new_data = simple_lookup(val[i], embkey=rform['embd'])
+                    if 'results' in new_data:
+                        print(f"error in {rform['embd']}, found a new one with {val[i]}")
+                        data = new_data
+                        found = True
+                        break
+
     data['embd'] = rform['embd']
     resp = Response(json.dumps(data), status=200, mimetype='application/json')
     return resp
